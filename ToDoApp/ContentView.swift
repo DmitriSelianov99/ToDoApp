3d9cbf979597fd38d3359f355028b69da687d3cb
DmitriSelianov99 <dimin712@mail.ru>
2024-11-19 14:01:08 +0500

    Add SearchView2

--- ToDoApp/ContentView.swift
+++ ToDoApp/ContentView.swift
@@ -6,6 +6,9 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var textfieldText: String = ""
+    
     var body: some View {
         ZStack {
             
@@ -13,6 +16,8 @@ struct ContentView: View {
             
             VStack(alignment: .leading, spacing: 10.0) {
                 TitleView()
+                
+                SearchView(textfieldText: $textfieldText)
             }
             .padding(.horizontal, 20)
         }
@@ -28,5 +33,38 @@ struct TitleView: View {
          Text("_tasks")
             .foregroundStyle(.customWhite)
             .font(.system(size: 34, weight: .bold))
+    }
+}
+
+struct SearchView: View {
+    
+    @Binding var textfieldText: String
+    
+    var body: some View {
+        ZStack {
+            Color.customGray
+            
+            HStack {
+                Image(systemName: "magnifyingglass")
+                    .renderingMode(.template)
+                    .foregroundStyle(.customWhite.opacity(0.5))
+                    .font(.system(size: 17, weight: .regular))
+                
+                TextField(text: $textfieldText) {
+                    Text("_search")
+                        .foregroundStyle(.customWhite.opacity(0.5))
+                        .font(.system(size: 17, weight: .regular))
+                }
+                .foregroundStyle(.customWhite)
+                
+                Image(systemName: "mic.fill")
+                    .renderingMode(.template)
+                    .foregroundStyle(.customWhite.opacity(0.5))
+                    .font(.system(size: 17, weight: .regular))
+            }
+            .padding()
+        }
+        .frame(height: 36)
+        .clipShape(RoundedRectangle(cornerRadius: 10))
     }
 }
